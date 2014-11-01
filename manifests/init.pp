154658be24b859f897362c0ca860a45374ffa25c
Alex <alexander.kardos@gmail.com>
2014-11-01 01:06:05 -0600

    Separated install.pp into two OS files
    The previous config only supported Debian. Ubuntu support was close, but
    the launchpad repository has slightly different package names. By separating
    into two OS files I've allowed for further configuration. The default
    package for Ubuntu is currently 1.1 and is sourced from launchpad as the
    current package in Ubuntu's repository (0.3) is unacceptably dated.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,5 +2,5 @@ class weechat (
   $install_upstream_packages = $weechat::params::install_upstream_packages,
   $install_devel_packages    = $weechat::params::install_devel_packages,
 ) inherits weechat::params {
-  include weechat::install
+  include "weechat::$::operatingsystem"
 }
